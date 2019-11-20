ecfcd77af115645ba4da353b00d9ef826b863426
Steffen Christgau <mail@s14u.de>
2019-11-20 19:19:29 +0100

    feat(main): Integrate task framework and control plane.

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <driver_init.h>
 
+#include "rtc.h"
+#include "tasks.h"
+
+#include "control.h"
+
 int main(void)
 {
-	/* Initializes MCU, drivers and middleware */
 	system_init();
+	task_init();
+	control_init();
 
-	/* Replace with your application code */
 	while (1) {
+		/* wake up after one second if scheduler does not say otherwise */
+		rtc_time_t next_run = rtc_current_time + 1000;
+
+		task_schedule(&next_run);
+
+		/*sleep_until(next_run);*/
 	}
 }
